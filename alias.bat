bb09a9856c5b6059d472d9b87deecb16d0675749
alittlemc <43811017+alittlemc@users.noreply.github.com>
2022-10-07 03:26:14 +0800

    Windows下的alias,可选的功能

---
+++ alias.bat
@@ -0,0 +1,18 @@
+﻿@echo off
+echo alittlemc 杨泽澳
+doskey ls=dir /b $*
+doskey ll=dir /ONE $*
+doskey cat=type $*
+doskey rm=del $*
+doskey mv=move $*
+doskey cd=cd /d $*
+doskey pwd=cd
+doskey mkdir=md $*
+doskey traceroute=tracert $*
+doskey tracepath=pathping $*
+doskey ifconfig=ipconfig $*
+::doskey findstr=grep $*
+doskey shell=PowerShell $*
+doskey history=doskey  /history
+doskey alias=doskey /macros
+echo 别名完成载入，键入alias查看
